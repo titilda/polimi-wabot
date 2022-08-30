e0bf0f562a87935e495e52efaf4bd9ce8d00912b
TheManchineel <37479927+TheManchineel@users.noreply.github.com>
2022-08-30 23:31:44 +0200

    feat: 🔒️ Add timestamp and blacklist feature for /everyone

--- commands.js
+++ commands.js
@@ -1,5 +1,6 @@
 var { nconf } = require('./config.js');
 var { addMemberToGroup, removeMemberFromGroup, getGroups, getGroupMembers, getGroupsWithMember } = require('./groups.js');
+var { getRateLimit } = require('./timestamp.js');
 
 const DISCORD_URL = nconf.get('DISCORD_URL');
 
@@ -233,8 +234,23 @@ async function pingCommand(client, message, args) {
 
 async function everyoneCommand(client, message, args) {
     const chat = await message.getChat()
+    const chat_id = chat.id._serialized;
     const quotedMessage = await message.getQuotedMessage();
     const participants = chat.participants;
+    const contact = await message.getContact();
+
+    // check if the user is a dumbass
+    if (nconf.get("BLACKLIST").includes(contact.id.user)) {
+        await message.reply("Mi spiace, ma visto che fai il pirla non ti affiderei manco le mie piante");
+        return;
+    };
+
+    // check if chat has /everyone ratelimited
+    if (getRateLimit("everyone", chat_id, nconf.get("RATE_LIMIT_EVERYONE"))) {
+        await message.reply(`Mi spiace, si può usare il comando solo ogni ${nconf.get("RATE_LIMIT_EVERYONE")} secondi`);
+        return;
+    };
+
     let pingDict = {};
     for (let participant of participants) {
         if (participant.id.user != client.info.wid._serialized.split("@")[0]) {
