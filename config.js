13933eb84e6425bbc086496043c80637c2afe4eb
Manchineel <manchineel@infinitedesert.com>
2022-10-13 13:56:10 +0200

    ci: 🔨 Add dev environment

--- config.js
+++ config.js
@@ -1,6 +1,20 @@
 var nconf = require('nconf');
 
-nconf.use('file', { file: './data.json' });
+var configFile
+
+// Check if ENV is production or development
+switch (process.env.NODE_ENV) {
+    case 'production':
+        configFile = './data.json';
+        break;
+    case 'development':
+        configFile = './data.dev.json';
+        break;
+    default:
+        configFile = './data.dev.json';
+}
+
+nconf.use('file', { file: CONFIG_FILE });
 nconf.load();
 
 module.exports = {
--- start.dev.sh
+++ start.dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-$ENV=production
+$ENV=development
 cd "$(dirname "$0")"
 node ./app.js
