7a9211b4a0c568c3c40173f0726f6fafd6aa2e30
TheManchineel <37479927+TheManchineel@users.noreply.github.com>
2023-12-15 15:12:24 +0100

    chore: 🙈 Remove spurious test file

--- test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-
-app = express();
-
-app.get("/", (req, res) => { try {res.send(undefined.hello)} catch { res.send("<html><body><marquee>FUCK OFFF</marquee></body></html>")} } );
-app.listen(4000);
-
